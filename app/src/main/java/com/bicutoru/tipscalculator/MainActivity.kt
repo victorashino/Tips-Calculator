be2183b40b86cdf92cc9dee4e80b08075a45019c
Victor Ashino <victor.ashino.main@gmail.com>
2024-08-17 17:57:03 -0300

    TipCalculator

--- app/src/main/java/com/bicutoru/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/bicutoru/tipscalculator/MainActivity.kt
@@ -1,20 +1,79 @@
 package com.bicutoru.tipscalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bicutoru.tipscalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var percentage: Int = 0
+
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 10
+            }
+        }
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 15
+            }
+        }
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 20
+            }
+        }
+
+        binding.btnClean.setOnClickListener {
+            binding.tvResult.text = ""
+            binding.tieTotal.setText("")
+            binding.tieNumPeople.setText("")
+            binding.rbOptionOne.isChecked = false
+            binding.rbOptionTwo.isChecked = false
+            binding.rbOptionThree.isChecked = false
+        }
+
+        binding.btnDone.setOnClickListener {
+            val totalTableTemp = binding.tieTotal.text
+            val nPeopleTemp = binding.tieNumPeople.text
+
+            if (totalTableTemp?.isEmpty() == true ||
+                nPeopleTemp?.isEmpty() == true
+            ) {
+                Snackbar
+                    .make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+
+                val totalTable: Float = binding.tieTotal.text.toString().toFloat()
+                val nPeople: Int = binding.tieNumPeople.text.toString().toInt()
+
+                val totalTemp = totalTable / nPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+                binding.tvResult.text = "Total with tips: $totalWithTips"
+            }
+
         }
     }
 }
